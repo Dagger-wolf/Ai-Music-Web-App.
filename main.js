8812de951c0ea0da8d34f5ea8bc96b385022b4a1
Dagger_Wolf#983 <93092770+Dagger-wolf@users.noreply.github.com>
2022-04-20 17:40:48 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+leftwristx = 0;
+rightwristx = 0;
+leftwristy = 0;
+rightwristy = 0;
+
+function setup(){
+
+    canvas = createCanvas(600,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    posenet = ml5.poseNet(video, modeLoaded);
+    posenet.on('pose', gotposes);
+
+}
+
+function draw(){
+
+    image(video, 0, 0, 600,500);
+
+}
+
+sound1 = "";
+sound2 = "";
+
+function preload(){
+
+    sound1 = loadSound("mp3.mp3");
+    sound2 = loadSound("music.mp3");
+
+}
+
+function modeLoaded(){
+
+    console.log("Model loaded");
+
+}
+
+function gotposes(results){
+
+    if(results.length > 0){
+
+        console.log(results);
+
+        leftwristx = results[0].pose.leftWrist.x;
+        leftwristy = results[0].pose.leftWrist.y;
+        rightwristx = results[0].pose.rightWrist.x;
+        rightwristy = results[0].pose.rightWrist.y;
+
+        console.log("Left Wrist X = " + leftwristx + " Left Wrist Y = " + leftwristy + " Right Wrist X = " + rightwristx + " Right Wrist Y = " + rightwristy);
+
+    }
+
+}
